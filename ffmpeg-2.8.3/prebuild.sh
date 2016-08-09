cb1171ee0f9de639368e42a3ac37b24cc636614d
Xianleewu <Xianleewu@github.com>
2016-08-09 09:54:18 +0800

    ffmpeg config file

---
+++ ffmpeg-2.8.3/prebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+./configure \
+--enable-shared \
+--disable-static \
+--disable-doc \
+--enable-gpl \
+--enable-nonfree \
+--enable-libx264 \
+--enable-libfreetype \
+--enable-filter=drawtext
